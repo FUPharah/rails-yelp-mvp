f37b4bda63933f514a4532f6770570acd7a2f961
canozben <canozben@windowslive.com>
2023-02-22 11:38:45 +0100

    models

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,15 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'reviews/new'
+  resources :restaurants do
+    collection do
+      get :top
+    end
+    member do
+      get :chef
+    end
+    resources :reviews, only: [:new, :create]
+  end
+  resources :reviews, only: [:destroy]
 end
